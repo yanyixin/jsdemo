42a15a4705b502a48d6fb2de43293c0bb52da4c5
sunshine940326 <991939332@qq.com>
2017-05-18 23:43:12 +0800

    增加demo js进行增删改查

--- 简单纯js实现网页tab选项卡切换效果/main.js
+++ 简单纯js实现网页tab选项卡切换效果/main.js
@@ -1,26 +1,28 @@
 /**
  * Created by supfn on 2017/5/18.
  */
-var order;
-var inputValue;
-function inputLinstner() {
-    inputValue = $('#input').val();
-}
-function click() {
-    var _this = this;
-    order = $(_this).attr("data-index");
-    $('.input-mask').show()
-}
-var items = document.getElementsByClassName('item');
-var item = [];
-$('#button').on('click',function () {
-    for(var i=0;i<items.length;i++){
-        if(items[i].getAttribute("data-index") == order){
-            items[i].getElementsByTagName('p')[0].innerHTML=inputValue
-        }
+window.onload=function () {
+    var order;
+    var inputValue;
+    function inputLinstner() {
+        inputValue = $('#input').val();
     }
-    $('#input').val("")
-    $('.input-mask').hide()
-})
-$('.item').on('click',click)
-$('#input').on('input',inputLinstner)
+    function click() {
+        var _this = this;
+        order = $(_this).attr("data-index");
+        $('.input-mask').show()
+    }
+    var items = document.getElementsByClassName('item');
+    var item = [];
+    $('#button').on('click',function () {
+        for(var i=0;i<items.length;i++){
+            if(items[i].getAttribute("data-index") == order){
+                items[i].getElementsByTagName('p')[0].innerHTML=inputValue
+            }
+        }
+        $('#input').val("")
+        $('.input-mask').hide()
+    })
+    $('.item').on('click',click)
+    $('#input').on('input',inputLinstner)
+}
