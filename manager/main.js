3aac4cf8d9f746fb9cc0acf9f727f042053b72d0
sunshine940326 <991939332@qq.com>
2017-05-22 22:55:27 +0800

    增加checkbox demo

--- manager/main.js
+++ manager/main.js
@@ -3,134 +3,116 @@
  */
 window.onload=function () {
 
-    var order,
-        inputValue,
-        ulInputValue,
+    var inputValue,
+        ulInputValue = $('#ul-input').val(),
         beforeInputVal,   //保存输入前的值
-        oItem = document.getElementsByClassName('item'),
-        item = [];
-
-    var ulList1 = document.getElementById('ul-list1');
+        item = [],
+        ulList1 = document.getElementById('ul-list1');   //获取限时优惠目的地列表
+    //使用sortable插件进行拖动
     if(ulList1){
         var sortable = Sortable.create(ulList1);
     }
-
-    var UlInputValue3 = $('#ul-input3').val();
     //记录输入框中的值
     function inputLinstner() {
         inputValue = $('#input').val();
         ulInputValue = $('#ul-input').val();
-        UlInputValue2 = $('#ul-input2').val();
-        UlInputValue3 = $('#ul-input3').val();
-
     }
 
+    function addClass(ele,name) {
+        $(ele).addClass(name)
+    }
+    function click(ele,func) {
+        $(ele).on('click',func)
+    }
+    function input(ele,func) {
+        $(ele).on('input',func)
+    }
+
+    function eventBind() {
+        click('.item',desModity)
+        click('#button',addDesBtnClick)
+        input('#input',inputLinstner)
+        input('#ul-input',inputLinstner)
+        click('#insertEnd',insertEndclick)
+        click('#ul-sure-button',ulSureBtnClick)
+        click('.delete-btn',deleteLi)
+        click('.modify-btn',modifyFun)
+    }
+    eventBind()
     //点击单个div，将输入框中的值复制给点击div中的p标签
-    function click() {
+    function desModity() {
         var _this = this;
-        order = $(_this).attr("data-index");
-        beforeInputVal = oItem[order - 1].getElementsByTagName('p')[0].innerHTML;
+        addClass(_this,'current');
+        beforeInputVal = $('_this').find('p').html();
         $('.input-mask').show()
+
     }
     //点击确定按钮
-    $('#button').on('click',function () {
-        for(var i=0;i<oItem.length;i++){
-            if(oItem[i].getAttribute("data-index") == order){
-                if( inputValue == undefined){
-                    oItem[i].getElementsByTagName('p')[0].innerHTML=beforeInputVal;
-                }else{
-                    oItem[i].getElementsByTagName('p')[0].innerHTML=inputValue
-                }
-            }
+    function addDesBtnClick() {
+        //判断是否输入内容
+        if(inputValue == undefined){
+            console.log(inputValue)
+            alert('请输入您要修改的目的地')
+            $('.item_container').find('.current').find('p').html(beforeInputVal)
+        }else{
+            console.log(inputValue)
+            $('.item_container').find('.current').find('p').html(inputValue)
         }
         $('#input').val("")
         $('.input-mask').hide()
-    })
-    $('.item').on('click',click)
-    $('#input').on('input',inputLinstner);
-    $('#ul-input').on('input',inputLinstner);
-    $('#ul-input2').on('input',inputLinstner);
-    $('#ul-input3').on('input',inputLinstner);
-
-
-
-    var selectLi;       //定义变量，保存选中的标签
-    var editLi = false; //保存修改后的标签
-    $('#insertEnd').on('click',insertEndclick);
-    $('#ul-sure-button').on('click',ulSureBtnClick);
-    $('#ul-sure-button2').on('click',ulSureBtnClick2);
-    $('#ul-sure-button3').on('click',ulSureBtnClick3);
-    $('#delete').on('click',deleteLi);
-    $('#edit').on('click',editLifun)
-    var ulList1 = document.getElementById('ul-list1')
-    if(ulList1){
-
+        $('.item_container').find('.current').removeClass('current');
+        inputValue = undefined;
     }
-
-
+    //增加
     function insertEndclick() {
+        $('.ul-input-container>p').html("新增目的地")
+        $('#ul-list1').addClass('addBtnClick')
         $('.ul-input-mask').show();
     }
-    //新增li至末位
+    //点击弹出框的确定按钮
     function ulSureBtnClick() {
+        $('#ul-input').val("")
+        //判断用户是否输入
         if(ulInputValue){
-            var newLi = document.createElement('li');
-            newLi.innerHTML =" <p> " + ulInputValue + "</p> " +
-                             " <div class=\"icon\"> " +
-                             "<img src=\"img/delete.png\" alt=\"删除\" class=\"delete-btn\" >"+
-                             "<img src=\"img/modify.png\" alt=\"修改\" class=\"modify-btn\">" +
-                             "</div>";
-            $('#ul-list1').append(newLi)
-        }else{
-            alert('请输入要插入内容')
+            //判断增加目的地还是修改目的地
+            if($('#ul-list1').hasClass('addBtnClick')){
+                var newLi = document.createElement('li');
+                newLi.innerHTML =" <p> " + ulInputValue + "</p> " +
+                    " <div class=\"icon\"> " +
+                    "<img src=\"img/delete.png\" alt=\"删除\" class=\"delete-btn\" >"+
+                    "<img src=\"img/modify.png\" alt=\"修改\" class=\"modify-btn\">" +
+                    "</div>";
+                $('#ul-list1').append(newLi)
+            }else if($('#ul-list1').hasClass('modifyBtnClick')){
+                $('#ul-list1').find('.current').find('p').html(ulInputValue);
+
+            }
+        } else{
+            alert('请输入目的地')
         }
+        $("#ul-list1").removeClass('modifyBtnClick')
+        $("#ul-list1").removeClass('addBtnClick')
         $('.ul-input-mask').hide();
-        $('.modify-btn').on('click',editLifun)
+        $('.modify-btn').on('click',modifyFun)
         $('.delete-btn').on('click',deleteLi)
+        $('#ul-list1').find('.current').removeClass('current');
+        ulInputValue = ""
     }
 
-    function ulSureBtnClick2() {
-
-        if(UlInputValue2){
-            var newLi = document.createElement('li');
-            newLi.innerHTML = UlInputValue2;
-            newLi.onclick = liChoose;
-            selectLi.before(newLi)
-        }else{
-            alert('请输入要插入内容')
-        }
-        $('.ul-input-mask2').hide();
-        /*for(var i=0;i<Oli.length;i++){
-         $(Oli[i]).removeClass('cur')
-         }*/
-    }
-
-    $('.delete-btn').on('click',deleteLi)
-    //删除指定标签
+    //删除
     function deleteLi() {
-        console.log("shanchu")
         var _this = this;
         $(_this).parent().parent().remove();
-
     }
 
-    $('.modify-btn').on('click',editLifun)
-    //修改指定标签
-    function editLifun() {
+    //修改
+    function modifyFun() {
+        $("#ul-list1").addClass('modifyBtnClick')
         $(this).parent().parent().addClass('current')
-        $('.ul-input-mask3').show();
+        $('.ul-input-mask').show();
 
     }
-    function ulSureBtnClick3() {
-        if(UlInputValue3){
-            $('#ul-list1').find('.current').find('p').html(UlInputValue3);
-        }else{
-            alert('请输入要修改的内容')
-        }
-        $('.ul-input-mask3').hide();
-        $('#ul-list1').find('.current').removeClass('current')
 
-    }
 
 }
 /**
